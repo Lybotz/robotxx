dacf1898ae57b7737c5b66a2db09c71b40a9bf06
lybotz <lybotics@gmail.com>
2019-09-25 00:38:18 +0200

    starting dictionary script

---
+++ robot.py
@@ -0,0 +1,15 @@
+
+zara = {"shirts":300, 'shorts':'500', 'coats':'100', 'shoes':'350', 'sox':'100'}
+
+'''for o, v in zara:
+    print(o, v)
+    print(zara)
+    print('shirts'[:])
+'''
+
+print(zara)
+print(zara['shorts'])
+
+zara['baby clothes'] = 900
+
+print(zara)
